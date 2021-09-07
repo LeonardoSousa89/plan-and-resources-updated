542f33c9ac47f13db3132f368eccb22974e111e6
Leozin89python <leozin069@gmail.com>
2021-09-07 17:32:38 -0300

    demo

---
+++ test/database/comandos.sql
@@ -0,0 +1,63 @@
+							DCL
+							---
+grupos de comandos:
+
+psql  -U postgres
+psql - U [usuario] -d postgres
+psql - U [usuario] -d [database]
+\l
+\dt
+\c
+\c [database]
+\c [database] [usuario]
+\q
+\du
+\dp
+CREATE USER [usuario];
+CREATE ROLE [usuario];
+CREATE ROLE [group];
+CREATE ROLE [group/usuario] WITH LOGIN PASSWORD [password];
+ALTER ROLE  [usuario] PASSWORD [password];
+ALTER ROLE  [usuario] WITH PASSWORD [password];
+ALTER ROLE  [usuario] SUPERUSER;
+ALTER ROLE  [usuario] WITH SUPERUSER;
+ALTER ROLE  [usuario] NOSUPERUSER PASSWORD [password];
+ALTER ROLE  [usuario] WITH NOSUPERUSER PASSWORD [password];
+ALTER TABLE [nome_da_tabela] OWNER TO [novo_dono];
+
+
+
+no database que se concederá os privilégios:
+
+GRANT [privilegios] ON [tabela] IN SCHEMA [schema] TO [role];
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO [usuario];
+REVOKE [privileges] ON [table] FROM [usuario];
+REVOKE INSERT, UPDATE, DELETE ON [table] FROM [usuario];
+
+
+
+                   	DDL
+CREATE DATABASE [database];
+DROP DATABASE [database];
+CREATE TABLE [table];
+ALTER TABLE [table];
+ALTER TABLE [table] DROP COLUMN [column]; 
+ALTER TABLE [table] ADD COLUMN  [column]; 
+DROP TABLE  [table];
+CREATE SCHEMA [schema];
+CREATE PROCEDURE [procedure];
+
+                    DML    
+INSERT INTO [table] VALUES(table_values);
+UPDATE  
+DELETE FROM [table] WHERE [condition];
+					DQL
+
+SELECT * FROM [table];
+SELECT [condition] FROM [table];
+SELECT [condition] FROM [table] WHERE [condition];
+
+referências:
+
+https://www.devmedia.com.br/gerenciando-usuarios-e-permissoes-no-postgresql/14301
+https://www.devmedia.com.br/controle-de-acesso-no-postgresql-9-1/25392
---
+++ test/database/knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgres',
+    connection: {
+      database: 'test',
+      user:     'userteste1',
+      password: '123456'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};

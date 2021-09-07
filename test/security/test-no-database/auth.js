3731591a936843a81c2d9ab4176420fafe78605f
Leozin89python <leozin069@gmail.com>
2021-09-07 20:43:16 -0300

    demo

--- test/security/test-no-database/auth.js
+++ test/security/test-no-database/auth.js
@@ -4,5 +4,20 @@ bcrypt.hashSync()
 const users = [{
     _id: 1,
     username: 'adm',
-    password: ''
+    password: 'gUuFwNo4zkMV+erdGtBlf5NunNgcELQuiCFJmCU4F+E=',
+    email:    'user@gmail.com'
 }]
+
+export default function User(passport){
+    function findUser(userName){
+        return users.find(item => item.username === username)
+    }
+
+    function findUserById(id){
+        return users.find(item => item.id === id)
+    }
+
+    passport.serializeUser((user, done)=>{
+        done(err, user._id)
+    })
+}

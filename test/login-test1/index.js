d4154f0ef99ecb80cbda9b303ae2995430f0ccb6
Leozin89python <leozin069@gmail.com>
2021-09-08 18:54:36 -0300

    demo

--- test/login-test1/index.js
+++ test/login-test1/index.js
@@ -1,32 +1,59 @@
 import express from 'express'
 import log     from 'morgan'
 import cors    from 'cors'
+import  db     from './data.json' 
 
-const port = 3004
-const app  = express()
+const port      = 3004
+const app       = express()
 app.use(log('dev'))
 app.use(cors({ origin:'*' }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-let userdb = 'mendes'
-let passdb = '1234'
-
-
 app.post('/',(req,res,next)=>{
     
-    let login = {
-        user : req.body.user,
-        pass : req.body.pass
+    try{
+        
+       const { user, pass } = req.body
+
+       let login = {
+            user,
+            pass
+       }
+
+        let data = {
+            userdb: 'mendes',
+            passdb: '1234'
+        }
+        
+
+        if(     !login.user         || !login.pass 
+                || 
+                login.user === ''   || login.pass === ''
+                ||
+                login.user === null || login.pass === null ){
+                    res.status(404)
+                       .json({msg:'User not found!'})
+                    console.log(login.user)
+                    console.log(login.pass)
+        }else if(login.user != data.userdb
+                &&
+                login.pass  != data.passdb){
+                     res.status(401)
+                        .json({msg:'Unauthorized!'})
+                    console.log(login.user)
+                    console.log(login.pass)
+        }else if(login.user === data.userdb
+                &&
+                login.pass  === data.passdb){
+                     res.redirect(200,'http://localhost:3003/')
+                       
+        }
+
+    }catch(err){
+        res.redirect('http://localhost:3003/login')
     }
 
-    if(login.user != userdb && login.pass != passdb) return res.redirect('http://localhost:3003/login')    
-
-    else next()
-})
-
-app.get('/',(req,res)=>{
-    res.redirect('http://localhost:3003/')    
 })
 
 app.listen(port,()=>console.log(`online into port:${port}`))
--- test/login-test1/server.js
+++ test/login-test1/server.js
@@ -2,6 +2,11 @@ import express  from 'express'
 import log      from 'morgan'
 import cors     from 'cors'
 
+function block(req,res){
+    if(req.statusCode === '200') return res.redirect('/')
+    return res.redirect('/login')
+}
+
 const port = 3003
 const app  = express()
 app.use(log('dev'))
@@ -11,7 +16,7 @@ app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/',(req,res)=>{
+app.get('/',block,(req,res)=>{
     res.sendFile(__dirname + '/app.html')
 })
 

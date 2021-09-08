3399963d4c9a1acf5ce6b54c667ee798dd0b8ad7
Leozin89python <leozin069@gmail.com>
2021-09-07 22:10:29 -0300

    demo

--- test/login/server.js
+++ test/login/server.js
@@ -3,15 +3,20 @@ import express from 'express'
 const port = 3003
 const app = express()
 
+/** melhorar a lógica */
+function block(req,res,next){
+    res.redirect('/')
+}
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/auth',(req,res)=>{
+app.get('/auth', block, (req,res)=>{
     res.sendFile(__dirname + '/logado.html')
 })
 
-app.get('/err',(req,res)=>{
+app.get('/err', (req,res)=>{
     res.sendFile(__dirname + '/assets/404.gif')
 })
 
